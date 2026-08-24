1473320bd6b13f460edcde7d76d8675c8071f435
Ratanmani <ratanmanisingh553@gmail.com>
2026-08-25 00:39:12 +0530

    characters in a order

--- Practice/star_patterns.cpp/right_angle.cpp
+++ Practice/star_patterns.cpp/right_angle.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 using namespace std;
 
-// int main(){
-//     int n;
-//     cout<<"Enter any number: ";
-//     cin>>n;
-//     for (int i=0;i<n;i++){
-//         for (int j=0;j<=i;j++){
-//             cout<<"*";
-//         }
-//         cout<<endl;
-//     }
-// }
+// 1. Right angle triangle.
+
+int main(){
+    int n;
+    cout<<"Enter any number: ";
+    cin>>n;
+    for (int i=0;i<n;i++){
+        for (int j=0;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+
+// 2. ABC in the form of Right angle triangle.
 
 int main()
 {
@@ -26,6 +31,23 @@ int main()
             cout << ch;
             ch++;
         }
+        cout << endl;
+    }
+}
+
+// 3. ABC with a sequence where every character with respect of line of the code.
+
+int main()
+{
+    int n;
+    cout << "Enter any Number: ";
+    cin >> n;
+    char ch = 'A';
+    for (int i=0;i<n;i++){
+        for (int j=0;j<=i;j++){
+            cout<<ch;
+        }
+        ch++;
         cout << endl;
     }
 }
