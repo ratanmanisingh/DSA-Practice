a55915457e7ecdacb015e53eb364f00e28d5de1a
Ratanmani <ratanmanisingh553@gmail.com>
2026-08-29 23:43:53 +0530

    Full Pyramid

--- Practice/star_patterns.cpp/full_pyramid.cpp
+++ Practice/star_patterns.cpp/full_pyramid.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 using namespace std;
 
+// int main(){
+//     int n;
+//     cout << "Enter your number: ";
+//     cin>>n;
+//     for (int i=1;i<=n;i++){
+//         for (int j=1;j<=n-i;j++){
+//             cout<< " " ;
+//         }
+//         for (int k=1;k<=i;k++){
+//             cout<<"* ";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+//  output ==       *
+//                 * *
+//                * * *
+//               * * * *
+
 int main(){
     int n;
     cout << "Enter your number: ";
@@ -9,10 +30,15 @@ int main(){
         for (int j=1;j<=n-i;j++){
             cout<< " " ;
         }
-        for (int k=1;k<=i;k++){
-            cout<<"* ";
+        for (int k=1;k<=(2*i-1);k++){
+            cout<<"*";
         }
         cout<<endl;
     }
     return 0;
 }
+
+// Output ==     * 
+//              ***
+//             *****
+//            *******
