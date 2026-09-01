c6748348297eb62314c6437521a62c07fc67a708
Ratanmani <ratanmanisingh553@gmail.com>
2026-09-02 01:12:43 +0530

    Hollow Diamond

--- Practice/star_patterns.cpp/hollow_pyramid.cpp
+++ Practice/star_patterns.cpp/hollow_pyramid.cpp
@@ -2,5 +2,37 @@
 using namespace std;
 
 int main(){
-    
+    int n;
+    cout << "Enter your number: ";
+    cin >> n;
+    cout << endl;
+    for (int i=0;i<n;i++){
+        for (int j=0;j<n-i-1;j++)
+        {
+            cout << " ";
+        }
+        for (int k=0;k<2*i+1;k++){
+            if (k==0 || k==2*i){
+                cout<< "*";
+            }
+            else{
+                cout << " ";
+            }
+        }
+        cout<<endl;
+        }
+        for (int i=0;i<n-1;i++){
+            for (int j=0;j<i+1;j++){
+                cout<<" ";
+            }
+            for (int k=0;k<2*(n-i-1)-1;k++){
+                if (k==0 || k==2*(n-i-1)-2){
+                    cout<<"*";
+                }
+                else{
+                    cout<<" ";
+                }
+            }
+            cout<<endl;
+        }
 }
