f926840f4977549246c2b663a1264a50f2a06db9
Ratanmani <ratanmanisingh553@gmail.com>
2026-09-02 16:32:07 +0530

    first

--- Linked List/first.cpp
+++ Linked List/first.cpp
@@ -15,6 +15,13 @@ int main(){
     b->data = 4;
     b->next = NULL;
     node* head = a;
-     
+    // node* i = head;               //(By using while loop)   
+    // while (i!=NULL){
+    //     cout<<i->data<<endl;
+    //     i= i->next;
+    // }
+    for (node* i=head;i!=NULL;i=i->next){    //(By using for loop)
+        cout << i->data << endl;
+    }
     return 0;
 }
