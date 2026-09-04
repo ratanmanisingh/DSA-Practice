5c2a1cc2ed552ec2a114cca3272b40133937bf90
Ratanmani <ratanmanisingh553@gmail.com>
2026-09-04 11:03:50 +0530

    finding key

--- Practice/Linked List/transversal.cpp
+++ Practice/Linked List/transversal.cpp
@@ -6,17 +6,45 @@ struct node{
     node* next;
 };
 
-int main(){
-    node* a=new node();
-    node* b=new node();
-    a->data=9;
-    a->next=b;
-    b->data=4;
-    b->next=NULL;
-    node* head = a;
-    node* current=head;
-    while(current!=NULL){
-        cout<<current->data<<" ";
-        current = current->next;
+// int main(){
+//     node* a=new node();
+//     node* b=new node();
+//     a->data=9;
+//     a->next=b;
+//     b->data=4;
+//     b->next=NULL;
+//     node* head = a;
+//     node* current=head;
+//     while(current!=NULL){
+//         cout<<current->data<<" ";
+//         current = current->next;
+//     }
+// }
+
+// for finding key
+
+int main()
+{
+    node *a = new node();
+    node *b = new node();
+    a->data = 9;
+    a->next = b;
+    b->data = 4;
+    b->next = NULL;
+    node *head = a;
+    int key;
+    cin>>key;
+    node *current = head;
+    while (current != NULL)
+    {
+        if(current->data==key){
+            cout<<"Element present";
+            break;
+        }
+        else{
+            cout<<"Element not present";
+            break;
+        }
+        current=current->next;
     }
 }
