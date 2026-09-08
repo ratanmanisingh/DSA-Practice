f50ca0eb41629ad84d464fd81eeb86a43fffed85
Ratanmani <ratanmanisingh553@gmail.com>
2026-09-09 02:09:27 +0530

    searching algorithms

---
+++ Practice/2.Searching Algorithms/linear_search.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[10];
+    for (int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int key;
+    cin>>key;
+    for(int i=0;i<n;i++){
+        if(arr[i]==key){
+            cout<<i;
+            return 0;
+        }
+    }cout<<"Not found";
+}
