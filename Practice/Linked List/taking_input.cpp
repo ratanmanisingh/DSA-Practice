d4566546e3fdb69e4fa66b92942ac2ca2b8960d1
Ratanmani <ratanmanisingh553@gmail.com>
2026-09-08 13:24:59 +0530

    Taking and printing input

--- Practice/Linked List/taking_input.cpp
+++ Practice/Linked List/taking_input.cpp
@@ -13,6 +13,8 @@ int main()
     cin>>n;
     node* head= NULL;
     node* tail= NULL;
+
+    //Taking input
     for(int i=0;i<n;i++){
         node* newnode = new node();
         cout<<"Enter numbers: ";
@@ -23,10 +25,21 @@ int main()
         }tail->next=newnode;
         tail=newnode;
     }
+    //Printing the elements
     node* i=head;
     while(i!=NULL){
         cout<<i->data<<endl;
         i=i->next;
     }
+    
+    //For finding middle element
+    
+    node* slow=head;
+    node* fast=head;
+    while(fast!= NULL && fast->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    cout<<slow->data;
     return 0;
 }
