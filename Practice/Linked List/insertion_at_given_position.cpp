67751a8418c273d07db163a3f30e3379f1c69fb8
Ratanmani <ratanmanisingh553@gmail.com>
2026-09-10 01:03:11 +0530

    Insertion at a given Position

---
+++ Practice/Linked List/insertion_at_given_position.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+struct node
+{
+    int data;
+    node *next;
+};
+
+void insertPos(node *&head, int value, int pos)
+{
+    node *newnode = new node();
+    newnode->data = value;
+    node *current = head;
+
+    for (int i = 0; i < pos - 1; i++)
+    {
+        current = current->next;
+    }
+    newnode->next = current->next;
+    current->next = newnode;
+}
+
+int main()
+{
+    node *head = NULL;
+    insertPos(head, 100, 1);
+}
