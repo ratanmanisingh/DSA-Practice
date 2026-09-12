50a80d8f8a9e9d19764c95e6e50a8a111e896a45
Ratanmani <ratanmanisingh553@gmail.com>
2026-09-12 18:59:02 +0530

    Deletion at end

--- Practice/Linked List/deletion_at_end.cpp
+++ Practice/Linked List/deletion_at_end.cpp
@@ -12,14 +12,12 @@ void delpos(node *&head, int pos)
     if (head == nullptr)
         return;
 
-    if (pos == 1)
-    {
+    if (pos == 1){
         node *target = head;
         head = head->next;
         delete target;
         return;
     }
-
     node *temp = head;
 
     for (int i=1; i<pos-1 && temp->next !=nullptr; i++)
@@ -35,20 +33,17 @@ void delpos(node *&head, int pos)
     delete target;
 }
 
-void printList(node *head)
-{
-    while (head != nullptr)
-    {
+void printList(node *head){
+    while (head != nullptr){
         cout << head->data << " ";
         head = head->next;
     }
 }
 
-int main()
-{
+int main(){
     node *a = new node{10, nullptr};
     node *b = new node{20, nullptr};
-
+    
     a->next = b;
     node *head = a;
 
