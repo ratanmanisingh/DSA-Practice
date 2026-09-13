775343abb9f86d34c4b60e2964d06b96bd3584ec
Ratanmani <ratanmanisingh553@gmail.com>
2026-09-14 03:57:08 +0530

    Insertion at Given Position

--- Practice/4.Linked List/insertion_at_given_position.cpp
+++ Practice/4.Linked List/insertion_at_given_position.cpp
@@ -21,9 +21,55 @@ void insertPos(node *&head, int value, int pos)
     current->next = newnode;
 }
 
+// second method
+
+// Node *insertAtPosition(Node *head, int value, int pos)
+// {
+//     Node *newNode = new Node();
+//     newNode->data = value;
+
+//     if (pos == 1)
+//     {
+//         newNode->next = head;
+//         return newNode;
+//     }
+
+//     Node *temp = head;
+//     for (int i = 1; i < pos - 1 && temp != NULL; i++)
+//     {
+//         temp = temp->next; // reach node before target position
+//     }
+
+//     newNode->next = temp->next;
+//     temp->next = newNode;
+
+//     return head;
+// }
+
 int main()
 {
-    node *head = NULL;
-    insertPos(head, 100, 1);
-    
+    node *a = new node();
+    node *b = new node();
+    node *c = new node();
+
+    a->data = 10;
+    b->data = 20;
+    c->data = 30;
+
+    a->next = b;
+    b->next = c;
+    c->next = NULL;
+
+    node *head = a;
+
+    insertPos(head, 15, 1);
+
+    node *i = head;
+
+    while (i != NULL)
+    {
+        cout << i->data << " ";
+        i = i->next;
+    }
+    return 0;
 }
