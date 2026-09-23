cf9e86252891ea14294a69767e4c9bafa25736a2
Ratanmani <ratanmanisingh553@gmail.com>
2026-09-24 02:28:51 +0530

    Backward traversal

---
+++ Practice/4.Linked List/Doubly Linked list/backward_traversal.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+struct node
+{
+    int data;
+    node *next;
+    node *prev;
+};
+
+void traverseBackward(node* head){
+    if (head==NULL){
+        return;
+    }
+    node* i = head;
+    while(i->next!=NULL){
+        i=i->next;
+    }
+    while(i!=NULL){
+        cout<<i->data<<" ";
+        i=i->prev;
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter number of node: ";
+    cin >> n;
+    node *head = NULL;
+    for (int i = 0; i < n; i++)
+    {
+        node *newnode = new node();
+        cout << "Enter element: ";
+        cin >> newnode->data;
+        newnode->next = NULL;
+        newnode->prev = NULL;
+
+        // Attach the new node after the current last node.
+        if (head == NULL)
+        {
+            head = newnode;
+        }
+        else
+        {
+            node *last = head;
+            while (last->next != NULL)
+            {
+                last = last->next;
+            }
+            last->next = newnode;
+            newnode->prev = last;
+        }
+    }
+    traverseBackward(head);
+}
