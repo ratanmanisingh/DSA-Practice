ebff8c6c0aacb42f411e629944e6ff0b32f6972b
Ratanmani <ratanmanisingh553@gmail.com>
2026-09-24 01:53:24 +0530

    First

---
+++ Practice/4.Linked List/Doubly Linked list/creating_first.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+struct node{
+    int data;
+    node* next;
+    node* prev;
+};
+
+int main(){
+    node* a = new node();
+    node* b = new node();
+    node* c = new node();
+
+    a->data=10;
+    b->data=20;
+    c->data=30;
+
+    a->next=b;
+    b->next=c;
+    c->next=NULL;
+
+    a->prev=NULL;
+    b->prev=a;
+    c->prev=b;
+    
+}
