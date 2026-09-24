34b8471fa63e2fa64632e80ea6eeef697407d86f
Ratanmani <ratanmanisingh553@gmail.com>
2026-09-25 00:35:45 +0530

    Insertion at Beginning

--- Practice/4.Linked List/Header Linked List/inserting_at_beggining.cpp
+++ Practice/4.Linked List/Header Linked List/inserting_at_beggining.cpp
@@ -4,29 +4,33 @@ using namespace std;
 struct node
 {
     int data;
-    node* next;
+    node *next;
 };
 
-void insertbegCHL(node* &header, node* &tail, int value)
+void insertbegCHL(node *&header, node *&tail, int value)
 {
-    node* newnode = new node();
+    node *newnode = new node();
     newnode->data = value;
     newnode->next = header->next;
     header->next = newnode;
-    tail->next = header;
+
+    if (tail == header)
+    {
+        tail = newnode;
+    }
 }
 
 int main()
 {
     int n;
     cin >> n;
-    node* header = new node();
-    header->data=0;
-    header->next=NULL;
-    node* tail=header;
+    node *header = new node();
+    header->data = 0;
+    header->next = NULL;
+    node *tail = header;
     for (int i = 0; i < n; i++)
     {
-        node* newnode = new node();
+        node *newnode = new node();
         cin >> newnode->data;
         newnode->next = NULL;
 
@@ -41,13 +45,14 @@ int main()
             tail = newnode;
         }
     }
-    node* i = header;
-    insertbegCHL(header,tail,6);
-    while (i != header && i!=NULL)
+    insertbegCHL(header, tail, 6);
+
+    node *i = header->next;
+    while (i != NULL)
     {
         cout << i->data << " ";
         i = i->next;
     }
-    cout<<endl;
+    cout << endl;
     return 0;
 }
